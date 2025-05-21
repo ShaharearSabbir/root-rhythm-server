21a8436348ae01f4b3645550732dbf265873eee8
ShaharearSabbir <imshaharear@gmail.com>
2025-05-21 15:54:45 +0600

    plant delete addded

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@rootrhythm.2xpaliy.mongodb.net/?retryWrites=true&w=majority&appName=rootRhythm`;
 
 const client = new MongoClient(uri, {
@@ -18,15 +21,13 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
 
-    // Collections
     const userCollection = client.db("RRDB").collection("users");
     const plantCollection = client.db("RRDB").collection("plants");
 
@@ -86,7 +87,7 @@ const run = async () => {
           },
         };
 
-        const options = { upsert: true }; // Creates document if it doesn't exist (optional)
+        const options = { upsert: true };
 
         const result = await plantCollection.updateOne(
           filter,
@@ -99,14 +100,18 @@ const run = async () => {
         res.status(500).send({ error: "Failed to update plant" });
       }
     });
+
+    app.delete("/plant/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await plantCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 };
 
 run().catch(console.dir);
-
-app.use(cors());
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Plant is growing");
