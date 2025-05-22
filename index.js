96c1dd2b3bcb97d097f4d08acaad345ed73c9829
ShaharearSabbir <imshaharear@gmail.com>
2025-05-23 02:05:26 +0600

    API addted for home page

--- index.js
+++ index.js
@@ -6,8 +6,31 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
+
+const allowedOrigins = ["http://localhost:5173", "https://root-rhythm.web.app"];
+
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+
+//   allowedHeaders: ["Content-Type", "Authorization"],
+
+//   optionsSuccessStatus: 204,
+// };
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true,
+  })
+);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@rootrhythm.2xpaliy.mongodb.net/?retryWrites=true&w=majority&appName=rootRhythm`;
 
@@ -21,9 +44,9 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
   try {
-    await client.connect();
+    // await client.connect();
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
@@ -44,6 +67,23 @@ const run = async () => {
       const query = { uid: uid };
       const result = await userCollection.findOne(query);
       console.log(result);
+      res.send(result);
+    });
+
+    // Home Page API
+
+    app.get("/home", async (req, res) => {
+      const categories = await categoryCollection.find().toArray();
+      const latestPlants = await plantCollection
+        .find()
+        .sort({ createdAt: -1 })
+        .limit(6)
+        .toArray();
+
+      const result = {
+        categories: categories,
+        latestPlants: latestPlants,
+      };
       res.send(result);
     });
 
@@ -72,12 +112,28 @@ const run = async () => {
 
     app.post("/plant", async (req, res) => {
       const plantData = req.body;
+      const createdAt = new Date();
+      plantData.createdAt = createdAt;
       const result = await plantCollection.insertOne(plantData);
       res.send(result);
     });
 
     app.get("/plants", async (req, res) => {
-      const result = await plantCollection.find().toArray();
+      const sortBy = req.query.sortBy;
+      const order = req.query.order === "descending" ? -1 : 1;
+      const sortOption = {};
+      sortOption[sortBy] = order;
+      const result = await plantCollection.find().sort(sortOption).toArray();
+      console.log(result);
+      res.send(result);
+    });
+
+    app.get("/latest", async (req, res) => {
+      const result = await plantCollection
+        .find()
+        .sort({ createdAt: -1 })
+        .limit(6)
+        .toArray();
       res.send(result);
     });
 
@@ -101,6 +157,8 @@ const run = async () => {
       try {
         const id = req.params.id;
         const plantUpdate = req.body;
+        const updatedAt = new Date();
+        plantUpdate.updatedAt = updatedAt;
         const filter = { _id: new ObjectId(id) };
 
         const updateDoc = {
